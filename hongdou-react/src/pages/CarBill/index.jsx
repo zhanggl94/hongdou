b0350a9404867a3e8ee950ea7a47a9fa5c29806a
zhanggl94 <zhanggl94@163.com>
2020-10-24 23:03:10 +0800

    添加汽车消费框架

--- hongdou-react/src/pages/CarBill/index.jsx
+++ hongdou-react/src/pages/CarBill/index.jsx
@@ -6,23 +6,6 @@ import constants from '../../utils/constants';
 
 const CarBill = () => {
 
-    api.carBill.searchRequest().then(data => {
-        if (data.isOk) {
-            console.log('carbill search success.');
-        } else {
-            openNotification({
-                type: constants.notifiction.type.error,
-                message: data.message
-            })
-        }
-    }).catch(error => {
-        console.log('error',error);
-        openNotification({
-            type: constants.notifiction.type.error,
-            message: error.message
-        })
-    })
-
     return (
         <CarBillView />
     );
--- hongdou-react/src/pages/Home/index.jsx
+++ hongdou-react/src/pages/Home/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { CarTwoTone } from '@ant-design/icons';
 
-const Home = ()=>{
-    return(
-        <div>Home</div>
+import './style.less';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div className='container'>
+            <ul class='anticons-list'>
+                <li className='TwoTone'>
+                    <Link to='/carbill'>
+                        <CarTwoTone />
+                        <span className='anticon-class'>
+                            <span className='ant-badge'>CarBill</span>
+                        </span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
     );
 }
 export default Home;
